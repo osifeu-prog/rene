782305d0faabc92fff420df8a05ac13ce6c7233d
Osif ungar <osif.e.u@gmail.com>
2025-11-19 00:26:00 +0200

    Create test.jsx

---
+++ pages/test.jsx
@@ -0,0 +1,8 @@
+export default function Test() {
+  return (
+    <div style={{ padding: 50, textAlign: 'center' }}>
+      <h1>🚀 TEST - האתר מתעדכן!</h1>
+      <p>אם אתה רואה זאת - ה-build עובד</p>
+    </div>
+  )
+}
